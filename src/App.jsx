d358050e619217abf21ae83202c1c2cbd6abc687
AHJimmy06 <jimmy.anilema234@gmail.com>
2025-01-05 01:57:38 -0500

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,6 @@ function App() {
             setEnergy((prev) => Math.max(0, prev - 10)); // Reducir energía
             setHappiness((prev) => Math.max(0, prev - 10)); // Reducir felicidad
         }, 1000); // Cada segundo
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/TamagotchiJavier
         if (health === 0) {
             const game = phaserRef.current.game;
             if (game) {
@@ -35,10 +31,6 @@ function App() {
                 game.scene.start('GameOver');
             }
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/TamagotchiJavier
         return () => clearInterval(interval); // Limpiar el intervalo al desmontar
     }, [health, food, energy, happiness]);
 
